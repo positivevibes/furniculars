f2dad0a944bc023c1aecce17df53f840f90b39f6
Abhijit <abhijit5887@gmail.com>
2012-03-04 12:58:46 +0530

    home page

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ Callingwood::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'user#about'
 
   # See how all your routes lay out with "rake routes"
 
